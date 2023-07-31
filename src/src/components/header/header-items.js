22bfd64b83893e5e1af566214bfa8bcb885edbaf
nknguyenhc <87511888+nknguyenhc@users.noreply.github.com>
2023-07-31 23:17:38 +0800

    Setup React & Sass

---
+++ src/src/components/header/header-items.js
@@ -0,0 +1,23 @@
+
+
+const headerItems = [
+    {
+        text: 'home',
+        url: '/'
+    },
+    {
+        text: 'projects',
+        dropdown: [
+            {
+                text: 'MatchMiner',
+                url: '/'
+            },
+            {
+                text: 'Coding Scheme Simulation',
+                url: '/'
+            }
+        ]
+    }
+];
+
+export default headerItems;
---
+++ src/src/components/header/header.jsx
@@ -0,0 +1,13 @@
+import headerItems from './header-items';
+
+export default function Header() {
+    return (
+        <header>
+            {headerItems.map((item, i) => (
+                item.url 
+                ? <a href={item.url} key={i}>{item.text}</a>
+                : <div key={i}>{item.text}</div>
+            ))}
+        </header>
+    );
+}

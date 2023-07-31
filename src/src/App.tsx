8c1b4f27afb4ba348d13b4756c4f0f1f2e8b1a43
nknguyenhc <87511888+nknguyenhc@users.noreply.github.com>
2023-08-01 00:03:04 +0800

    Router DOM Basics

--- src/src/App.tsx
+++ src/src/App.tsx
@@ -1,10 +1,12 @@
 import './App.scss';
+import { Outlet } from 'react-router-dom';
 import Header from './components/header/header';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Outlet />
     </div>
   );
 }
--- src/src/components/header/header.tsx
+++ src/src/components/header/header.tsx
@@ -1,11 +1,14 @@
 import headerItems from './header-items';
+import { Link } from 'react-router-dom';
 
 export default function Header(): JSX.Element {
     return (
         <header>
             {headerItems.map((item, i) => (
                 item.url 
-                ? <a className="header-item" href={item.url} key={i}>{item.text}</a>
+                ? <Link to={item.url}>
+                    <div className="header-item" key={i}>{item.text}</div>
+                </Link>
                 : <div className="header-item" key={i}>{item.text}</div>
             ))}
         </header>
